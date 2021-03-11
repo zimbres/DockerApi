47cd85040b991ffab452af47177e79d92c581179
Marcio <marcio@zimbres.com>
2021-03-11 18:02:39 -0300

    Add project files.

---
+++ DockerApi/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DockerApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class BaseController : ControllerBase
+    {
+        private readonly ILogger<BaseController> _logger;
+
+        public BaseController(ILogger<BaseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
